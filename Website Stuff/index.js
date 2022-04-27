ca0f087659fa485d71003910e96204b9a6cc3c9c
SleepyMonkey1 <98307462+SleepyMonkey1@users.noreply.github.com>
2022-04-27 15:35:34 +0200

    hey

--- Website Stuff/index.js
+++ Website Stuff/index.js
@@ -1,9 +1,19 @@
 
-const a = document.getElementById("test");
-
-a.innerHTML = 'haeh';
+const a = document.getElementById("test").innerHTML = "New text";
 
 
+function getHello() {
+    const url = 'http://localhost:8989/hello'
+    fetch(url)
+    .then(response => response.json())  
+    .then(json => {
+        console.log(json);
+        document.getElementById("demo").innerHTML = JSON.stringify(json)
+    })
+}
+
+
+getHello()
 
 
    
