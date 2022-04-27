f6adc0ef050932ccf28f169efce5b76d59bd34d8
Ke-Se <nstaubo@protonmail.com>
2022-04-27 12:57:00 +0200

    added function that can create tables
    just gotta write it in with SQlite code and then it should be good

--- main.py
+++ main.py
@@ -3,3 +3,8 @@ from multiprocessing import connection
 import sqlite3
 
 
+
+# Det er denne funksjonen som lager selve tablesene og putter dem i databasen.
+def create_tables(connection):
+    with connection:
+        connection.execute()
