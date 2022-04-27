23954f3fb050c7ed539f615d8a2177020dae5884
SigurdCiaran <98525110+SigurdCiaran@users.noreply.github.com>
2022-04-27 14:46:30 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,3 +5,15 @@ import sqlite3
 conn = sqlite3.connect('gay.db')
 
 c = conn.cursor()
+
+def pulling(x):
+    search_string = "SELECT * FROM ((table)) WHERE "
+    for term in x:
+        search_string += f"condition='{x}' AND "
+    search_string = search_string[:-5]
+    c.execute(search_string)
+    items = c.fetchall()
+    for item in items:
+        print(item)
+
+x = []
