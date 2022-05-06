cbb60c6c1333dd1402e7562c504c47b4e8d0c908
SigurdCiaran <98525110+SigurdCiaran@users.noreply.github.com>
2022-05-06 14:59:15 +0200

    did some stuff

--- Python Stuff/functions.py
+++ Python Stuff/functions.py
@@ -7,7 +7,7 @@ conn = sqlite3.connect('eksempel.db')
 c = conn.cursor()
 
 def pulling(search_list, output_list):
-    output_list = []
+    output_list.clear()
     search_string = "SELECT * FROM tasks WHERE "
     for term in search_list:
         search_string += f"{term} AND "
@@ -16,7 +16,7 @@ def pulling(search_list, output_list):
     items = c.fetchall()
     for item in items:
         output_list.append(item)
-    return output_list
+
 
 
 #   denne funksjonen finner items fra en liste med sql kommandoer i string format, og plasserer dem i en output liste
@@ -37,9 +37,4 @@ def pushing(insert_list):
 #   search_list må være en liste variabel med string items i formatet search_list = ["test_1 = 'eksempel1'", "test_2 = 'eksempel2'", "test_3 = 'eksempel3'"]
 #   output_list er en liste med itemsa funnet med pulling funksjonen. den cleares på starten av funksjonen, så ta med deg informasjonen til en annen variabel hvis du skal kjøre den flere ganger
 #   insert_list er en variabel av det du vil sette inn i tabelet. skriv det i formatet insert_list = ('eksempel1', 'eksempel2', 'eksempel3')
-insertlist = ('gaming', 'er', 'viktig')
-searchlist = ["test_1 = 'supremacy'"]
-outputlist = []
 
-pulling(searchlist, outputlist)
-print(outputlist)
