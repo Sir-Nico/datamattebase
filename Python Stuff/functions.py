05c7184ace5569bd8e2a8cf699c1f4ed92b97ff3
SigurdCiaran <98525110+SigurdCiaran@users.noreply.github.com>
2022-05-06 12:48:47 +0200

    jk

--- Python Stuff/functions.py
+++ Python Stuff/functions.py
@@ -6,36 +6,40 @@ conn = sqlite3.connect('eksempel.db')
 
 c = conn.cursor()
 
-def pulling(x, z):
+def pulling(search_list, output_list):
+    output_list = []
     search_string = "SELECT * FROM tasks WHERE "
-    for term in x:
+    for term in search_list:
         search_string += f"{term} AND "
- 
     search_string = search_string[:-5]
-    print("test")
     c.execute(search_string)
     items = c.fetchall()
     for item in items:
-        z.append(item)
+        output_list.append(item)
+    return output_list
 
-def pushing(y):
+
+#   denne funksjonen finner items fra en liste med sql kommandoer i string format, og plasserer dem i en output liste
+
+def pushing(insert_list):
     command_string = "INSERT INTO tasks VALUES ("
-    for term in y:
+    for term in insert_list:
         command_string += f"'{term}', "
     command_string = command_string[:-2]
     command_string += ")"
     c.execute(command_string)
     conn.commit()
     
+#   denne funksjonen inserter items fra en liste i tasks tabelet
 
 
 
-    
-#   z = []
-#   bytt z med en liste variabel der du vil at tingene du søker etter skal bli displaya (husk å cleare den)
+#   search_list må være en liste variabel med string items i formatet search_list = ["test_1 = 'eksempel1'", "test_2 = 'eksempel2'", "test_3 = 'eksempel3'"]
+#   output_list er en liste med itemsa funnet med pulling funksjonen. den cleares på starten av funksjonen, så ta med deg informasjonen til en annen variabel hvis du skal kjøre den flere ganger
+#   insert_list er en variabel av det du vil sette inn i tabelet. skriv det i formatet insert_list = ('eksempel1', 'eksempel2', 'eksempel3')
+insertlist = ('gaming', 'er', 'viktig')
+searchlist = ["test_1 = 'supremacy'"]
+outputlist = []
 
-#   y = ('something1', 'something2', 'something3')
-#   bytt med en variabel der something(1, 2, 3) er hva du vil ha i de respektive columnsa 
-
-#   x = ["test_1 = 'something1'", "test_2 = 'something2'", "test_3 = 'something3'"]
-#   bytt something(1, 2, 3) med en liste over columnsa, og hvilke item i de columnsa du vil søke etter
+pulling(searchlist, outputlist)
+print(outputlist)
