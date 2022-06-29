92c9f9bbe1243bc7f3ed164a5f66eb8ea10c10f1
SleepyMonkey1 <98307462+SleepyMonkey1@users.noreply.github.com>
2022-06-28 21:16:52 -0500

    push
    ppush

--- Database(connected_files)/main.py
+++ Database(connected_files)/main.py
@@ -1,36 +1,62 @@
-#create server
-##ballslover69 will create a server
 from ast import dump
 from crypt import methods
 from multiprocessing import connection
+from os import name
 import sqlite3
 
-from flask import Flask, request, jsonify, render_template, url_for
+conn = sqlite3.connect('eksempel.db')
+
+c = conn.cursor()
+
+from flask import Flask, request, jsonify, render_template, url_for,redirect, session, flash
+from datetime import timedelta
+
 
 app = Flask(__name__)
+app.secret_key = "hello" 
+app.permanent_session_lifetime = timedelta(days =5)
+
 
 @app.route("/", methods=['GET'])
 def home():
     return render_template("index.html")
-    #return "hey there"
     
+@app.route("/login", methods=["POST","GET"])
+def login():
+    if request.method == "POST":
+        session.permanent = True
+        user = request.form["nm"]
+        session["user"] = user
+        flash ("Login Succesfull!")
+        return redirect(url_for("user"))
+    else:
+        if "user" in session:
+            flash("Already Logged in")
+            return redirect(url_for("user"))
+
+        return render_template("login.html")
+
+@app.route("/user", methods= ["POST, GET"])
+def user():
+    email = None
+    if "user" in session:
+        user = session["user"]
+        if request.method == "POST":
+            email = request.form["email"]
+            session["email"] = email
+        return render_template("user.html", email =email)
+    else:
+        flash( "you are not logged in" )
+        return redirect(url_for("login"))
+
+
+@app.route("/logout")
+def logout():
+    flash("You have been logged out!", "info")
+    session.pop("user", None)
+    return redirect(url_for("login"))
+
 
 
 if __name__ == "__main__":
     app.run(debug = True)
-
-
-    
-    
-
-
-
-#conn = sqlite3.connect('gay.db')
-
-#c = conn.cursor()
-
-
-# Det er denne funksjonen som lager selve tablesene og putter dem i databasen.
-#def create_tables(conn):
-    #with connection:
-    #    conn.execute()
